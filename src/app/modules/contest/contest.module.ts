5150ce6f3d9edec925a046a0ed62da6924234e9c
Pavlo Sokolov <frontbastard@gmail.com>
2022-08-25 19:08:10 +0300

    [AC-28] Translations

--- src/app/modules/contest/contest.module.ts
+++ src/app/modules/contest/contest.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TRANSLOCO_SCOPE } from '@ngneat/transloco';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ContestRouterComponent } from './contest-router.component';
-import { ContestRoutingModule } from './contest-routing.module';
 import { ContestDetailsComponent } from './contest-details/contest-details.component';
 import { ContestListComponent } from './contest-list/contest-list.component';
+import { ContestRouterComponent } from './contest-router.component';
+import { ContestRoutingModule } from './contest-routing.module';
 
 @NgModule({
-  declarations: [ContestRouterComponent, ContestDetailsComponent, ContestListComponent],
+  declarations: [
+    ContestRouterComponent,
+    ContestDetailsComponent,
+    ContestListComponent,
+  ],
   imports: [CommonModule, SharedModule, ContestRoutingModule],
+  providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'contest' }],
 })
 export class ContestModule {}
